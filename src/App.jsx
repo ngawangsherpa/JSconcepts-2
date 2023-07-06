7815a9935ff646279432d8d6ab8721ae92887908
Ngawang P Sherpa <ngawangpsherpa@gmail.com>
2023-07-05 23:24:06 -0400

    Final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App =() => {
+  const products = [{name:'Cup',price:10,quantity:5},
+                    {name:'Table',price:50,quantity:3},
+                    {name:'Lamp',price:15,quantity:8}];
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+const calculateTotalPrice = (product) => {
+  return product.price * product.quantity;};
+
+const printProductInfo = (product) => {
+  console.log('Name:', product.name);
+  console.log('Price:', product.price);
+  console.log('Quantity:', product.quantity);
+  console.log('Total Price:', calculateTotalPrice(product));
+};
+return (
+  <div>
+    <h2>Product Information:</h2>
+    {products.map((product, index) => (
+      <div key={index}>
+        <h3>{product.name}</h3>
+        <p>Price: ${product.price}</p>
+        <p>Quantity: {product.quantity}</p>
+        <p>Total Price: ${calculateTotalPrice(product)}</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    ))}
+  </div>
+);
+};
+
 
 export default App
